f94b037aa591bb16566da73c1a57c3ad09bc12a4
Ramtin Kermani <ramtin@asu.edu>
2013-05-12 09:51:57 -0700

    Initial Commit for dataStructures Project

---
+++ linkedList.h
@@ -0,0 +1,56 @@
+#ifndef LINKEDLIST_H_INCLUDED
+#define LINKEDLIST_H_INCLUDED
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+typedef struct node Node;
+struct node{
+    int value;
+    Node* next;
+};
+
+void addToList(Node** head, int item){
+    Node* tempNode = (Node*) malloc(sizeof(Node));
+    tempNode->value = item;
+    tempNode->next = *head;
+    *head = tempNode;
+}
+
+void addToListEnd(Node** head, int item){
+    Node* itr = *head;
+    Node* tempNode = (Node*) malloc(sizeof(Node));
+    tempNode->value = item;
+    tempNode->next = NULL;
+    if(itr == NULL)
+        *head = tempNode;
+    else{
+        while(itr->next != NULL)
+            itr = itr->next;
+        itr->next = tempNode;
+    }
+}
+
+void printList(Node* head){
+    Node* itr = head;
+    while(itr->next != NULL){
+        cout << itr->value << endl;
+        itr = itr->next;
+    }
+    cout << itr->value << endl;
+}
+
+int listLen(Node* head){
+    Node* itr = head;
+    int len = 1;    // starting from 1 not zero because of the while condition for a one-element-list
+    if(itr == NULL) // if list is empty
+        return 0;
+    while(itr->next != NULL){
+        itr = itr->next;
+        len++;
+    }
+    return len;
+}
+
+#endif // LINKEDLIST_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include "linkedList.h"
+
+using namespace std;
+
+int main()
+{
+    Node* myList = NULL;
+    cout << "List Len: " << listLen(myList) << endl;
+    addToListEnd(&myList, 11);
+    cout << "List Len: " << listLen(myList) << endl;
+    addToListEnd(&myList, 12);
+    addToListEnd(&myList, 13);
+    addToListEnd(&myList, 14);
+    addToListEnd(&myList, 17);
+    addToList(&myList, 10);
+    cout << "List Len: " << listLen(myList) << endl;
+    printList(myList);
+
+
+    return 0;
+}
